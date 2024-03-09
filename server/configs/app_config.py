b0418d1dbfe116df2b8fd8a97638d442cedf4926
Alex Pervushin <alexpervushin@outlook.com>
2024-03-09 20:36:26 +0300

    Refactored app's configuration

--- server/configs/app_config.py
+++ server/configs/app_config.py
@@ -1,9 +1,26 @@
 import os
 
-server_address = os.environ.get("SERVER_ADDRESS")
-update_accounts_interval = 10
-websocket_interval = 1
+"""Configuration of the Melodymap server app
 
-path_prefix_end = "/"
+Contains the server address, the update accounts interval, the websocket
+interval, the path prefix end, and the maximum distance in meters to show
+accounts for the user.
 
-max_distance = 5
+The values are read from the environment variables, if they are not set the
+default values are used.
+"""
+
+server_address = os.environ.get("SERVER_ADDRESS", None)
+"""The server address to listen on, None for default"""
+
+update_accounts_interval = int(os.environ.get("UPDATE_ACCOUNTS_INTERVAL", 10))
+"""The interval in seconds to update the accounts from the Spotify API"""
+
+websocket_interval = int(os.environ.get("WEBSOCKET_INTERVAL", 1))
+"""The interval in seconds to send the accounts to the client through websocket"""
+
+path_prefix_end = os.environ.get("PATH_PREFIX_END", "/")
+"""The path prefix end to use for the server"""
+
+max_distance = int(os.environ.get("MAX_DISTANCE", 5))
+"""The maximum distance in meters to show accounts for the user"""
